0df36f3973fca9f9929980d65fbc8b3644bd0101
niklasburghardt <niklas.burghardt@web.de>
2022-02-20 15:49:39 +0100

    Updating models.py

--- upvote_backend/votables/models.py
+++ upvote_backend/votables/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Votable(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
+    images = models.FileField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Upvote(models.Model):
+     user = models.ForeignKey(User, on_delete=models.CASCADE)
+     votable = models.ForeignKey(Votable) #on delete user gets back paid upvotes
+     paid = models.IntegerField()
+     upvote_score = models.IntegerField()
+     active = models.BooleanField()
+     sold = models.IntegerField(null=True, blank=True)
+     created = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    votable = models.ForeignKey(Votable) #on delete user gets back paid upvotes
+    content = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
